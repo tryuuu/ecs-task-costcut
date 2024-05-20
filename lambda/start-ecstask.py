ab381124dda166c9007631e464a06a9fe3fbb4f8
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-05-20 18:44:17 +0900

    fix: lambda関数

--- lambda/start-ecstask.py
+++ lambda/start-ecstask.py
@@ -1,10 +1,16 @@
 import boto3
+import json
+from datetime import datetime
+
+def datetime_converter(o):
+    if isinstance(o, datetime):
+        return o.isoformat()
 
 def lambda_handler(event, context):
     client = boto3.client('ecs')
     response = client.update_service(
-        cluster='nippou-cluster',
-        service='nippou-service',
-        desiredCount=1  # タスク数を1にする
+        cluster='example-cluster',
+        service='example-service',
+        desiredCount=1  # タスク数を1にして再起動
     )
-    return response
+    return json.loads(json.dumps(response, default=datetime_converter))
--- lambda/stop-ecstask.py
+++ lambda/stop-ecstask.py
@@ -1,10 +1,16 @@
 import boto3
+import json
+from datetime import datetime
+
+def datetime_converter(o):
+    if isinstance(o, datetime):
+        return o.isoformat()
 
 def lambda_handler(event, context):
     client = boto3.client('ecs')
     response = client.update_service(
-        cluster='nippou-cluster',
-        service='nippou-service',
+        cluster='example-cluster',
+        service='example-service',
         desiredCount=0  # タスク数を0に設定して停止
     )
-    return response
+    return json.loads(json.dumps(response, default=datetime_converter))
